c8727fc806fc7a95d8fb2bc8b7de4c02da62b1f1
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2024-04-21 14:50:36 +0300

    Implement pyautogui to automatically make next best move on every run bot click

--- src/chessbot.py
+++ src/chessbot.py
@@ -1,60 +1,106 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from stockfish import Stockfish
 import chess
+import pyautogui
 
 class ChessBot:
   def __init__(self):
     self.driver = None
     self.board = chess.Board()
-    self.moves_list = []
     self.stockfish = Stockfish(path='../stockfish/stockfish-windows-x86-64-avx2.exe')
 
 
+  # open up a chrome browser
   def open_browser(self, event=None):
-    self.driver = webdriver.Chrome()
-
+    options = webdriver.ChromeOptions()
+    self.driver = webdriver.Chrome(options=options)
     self.driver.get('https://www.chess.com/play/computer/CoachDannyBot')
-
     input()
-
     self.driver.quit()
 
 
-  def get_moves(self):
-    self.moves_list = [] # reset list
+  # get the board element
+  def get_board(self):
+    return self.driver.find_element(By.XPATH, "//*[@id='board-play-computer']")
 
+
+  def get_square_center(self, square):
+    # get location of the top left of the website
+    site_pos_x = self.driver.execute_script("return window.screenX + (window.outerWidth - window.innerWidth) / 2 - window.scrollX;")
+    site_pos_y = self.driver.execute_script("return window.screenY + (window.outerHeight - window.innerHeight) - window.scrollY;")
+
+    # get location of the board
+    board_pos = self.get_board().location
+    board_pos_x = site_pos_x + board_pos["x"]
+    board_pos_y = site_pos_y + board_pos["y"]
+
+    # get the size of the squares
+    square_size = self.get_board().size['width'] / 8
+
+    column_to_index = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+    # extract the column and row from the square position (for example e2, column = e, row = 2)
+    column = square[0]
+    row = int(square[1])
+
+    # get the x,y int value for the corresponding square
+    x_index = column_to_index[column]
+    y_index = 8 - row
+
+    # get the exact coordinates for the center of the square
+    x_pos = board_pos_x + (square_size * x_index) + (square_size / 2)
+    y_pos = board_pos_y + (square_size * y_index) + (square_size / 2)
+
+    return (x_pos, y_pos)
+  
+
+  # click and drag piece to the next best position
+  def make_move(self, source, target):
+    print(source, target)
+    start_pos = self.get_square_center(source)
+    end_pos = self.get_square_center(target)
+
+    pyautogui.moveTo(start_pos[0], start_pos[1])
+    pyautogui.dragTo(end_pos[0], end_pos[1])
+
+
+  # get the real game updated/new moves and apply them to the internal board
+  def update_moves(self):
     move_container = self.driver.find_element(By.XPATH, '//*[@id="scroll-container"]/wc-vertical-move-list')
     moves = move_container.find_elements(By.CSS_SELECTOR, 'div.move [data-ply]')
     
-    for move in moves:
-      move_class = move.get_attribute("class")
+    new_moves = [move.text for move in moves if "white node" in move.get_attribute("class") or "black node" in move.get_attribute("class")]
+    last_known = len(self.board.move_stack)
 
-      if "white node" in move_class or "black node" in move_class:
-        self.moves_list.append(move.text)
-
-  
-  def get_best_move(self):
-    self.get_moves()
-    self.board.reset()
-
-    for move in self.moves_list:
+    # apply only new moves to the internal board
+    for move in new_moves[last_known:]:
       self.board.push_san(move)
 
-    move_list_uci = [move.uci() for move in self.board.move_stack]
+  
+  # get the next best move
+  def get_next_best_move(self):
+    self.update_moves()
 
-    self.stockfish.set_position(move_list_uci)
+    # set the position for stockfish with our new/updated internal board
+    moves_list_uci = [move.uci() for move in self.board.move_stack]
+    self.stockfish.set_position(moves_list_uci)
 
-    return self.stockfish.get_best_move()
+    # get the best move from stockfish given our current position
+    best_move = self.stockfish.get_best_move()
+    if best_move:
+      source = best_move[:2]
+      target = best_move[2:4]
+      return source, target
+    return None, None
 
   
   def run_bot(self, event=None):
-    best_move = self.get_best_move()
+    source, target = self.get_next_best_move()
 
-    print(best_move)
+    self.make_move(source, target)
+
+    print(f"Moved from {source} to {target}")
 
 
 
